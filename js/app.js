c8b3041ae9c6f0e90b4f687fd5f6bc5335b2aadf
TeChRF24 <f.rafael.temich24@gmail.com>
2022-08-04 12:46:58 -0500

    Done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-window.swiper = new Swiper({
-    el: '.slider__contenedor',
-    slideClass: 'slider__slide',
-    createElements: true,
-    autoplay: {
-        delay: 2000
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
     },
-     loop: true,
-     pagination: true,
-     navigation: true
-})
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    loop: true
+  });
